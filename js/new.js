5e8688a6f2ade5b7e87eeeef17ffb8ce47237848
jafabogaartha <138951954+jafabogaartha@users.noreply.github.com>
2025-05-03 08:13:11 +0700

    @jafabogaartha

--- js/new.js
+++ js/new.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('partnership-form');
     const lokasiUsahaSelect = document.getElementById('lokasi-usaha');
     const additionalAddressDiv = document.getElementById('additional-address');
@@ -8,21 +8,21 @@ document.addEventListener('DOMContentLoaded', function() {
     // Daftar admin
     const adminDetails = [{
 
-            number: '6285117276822',
-            name: 'Admin Berlian'
-        },
-           {
-            number: '6282124952606',
-            name: 'Admin Livia'
-        },
-        {
-            number: '6281234086100',
-            name: 'Admin Risma'
-        },
-        {
-            number: '6281212125422',
-            name: 'Admin Reka'
-        }
+        number: '6285117276822',
+        name: 'Admin Berlian'
+    },
+    {
+        number: '6282124952606',
+        name: 'Admin Livia'
+    },
+    {
+        number: '6281234086100',
+        name: 'Admin Risma'
+    },
+    {
+        number: '6281212125422',
+        name: 'Admin Reka'
+    }
     ];
 
     lokasiUsahaSelect.addEventListener('change', () => {
