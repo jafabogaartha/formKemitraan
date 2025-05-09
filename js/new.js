cfa7dfbbe4e241c54d89f861eeb4deba5afc037b
jafabogaartha <138951954+jafabogaartha@users.noreply.github.com>
2025-05-09 08:31:08 +0700

    @jafabogaartha

--- js/new.js
+++ js/new.js
@@ -8,8 +8,8 @@ document.addEventListener('DOMContentLoaded', function () {
     // Daftar admin
     const adminDetails = [
         {
-            number: '6285117276822',
-            name: 'Admin Berlian'
+            number: '6281234086100',
+            name: 'Admin Risma'
         },
         {
             number: '6282124952606',
@@ -21,8 +21,8 @@ document.addEventListener('DOMContentLoaded', function () {
             name: 'Admin Reka'
         },
         {
-            number: '6281234086100',
-            name: 'Admin Risma'
+            number: '6285117276822',
+            name: 'Admin Berlian'
         }
     ];
 
