5021825902072ee67534a101b3f2290932339669
jafabogaartha <138951954+jafabogaartha@users.noreply.github.com>
2025-06-03 08:19:25 +0700

    @jafabogaartha

--- js/new.js
+++ js/new.js
@@ -8,14 +8,18 @@ document.addEventListener('DOMContentLoaded', function () {
     // Daftar admin
     const adminDetails = [{
 
-            number: '6285117276822',
-            name: 'Admin Berlian'
-        },
+        number: '6285117276822',
+        name: 'Admin Berlian'
+    },
+    {
+        number: '6281212125422',
+        name: 'Admin Reka'
+    },
 
-        {
-            number: '6282124952606',
-            name: 'Admin Livia'
-        }
+    {
+        number: '6282124952606',
+        name: 'Admin Livia'
+    }
     ];
 
     lokasiUsahaSelect.addEventListener('change', () => {
