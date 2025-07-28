6f6e037c04fbb80416a0711db343919cd0ced2d6
jafabogaartha <138951954+jafabogaartha@users.noreply.github.com>
2025-07-28 14:44:46 +0700

    @jafabogaartha

--- js/new.js
+++ js/new.js
@@ -10,11 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         number: '6285117276822',
         name: 'Admin Berlian'
-    },
-    {
-        number: '6281212125422',
-        name: 'Admin Reka'
-    },
+    }
 
 
     ];
