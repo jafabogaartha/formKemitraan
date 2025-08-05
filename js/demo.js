dc7e6bd15b6efd6fd0582f55f29d39178dab24d8
jafabogaartha <138951954+jafabogaartha@users.noreply.github.com>
2025-08-05 09:07:21 +0700

    @jafabogaartha

--- js/demo.js
+++ js/demo.js
@@ -1,4 +1,5 @@
 const adminDetails = [
   { number: '6285117276822', name: 'Admin Berliana' },
+  { number: '6281212125422', name: 'Admin Reka' },
   { number: '6282124952606', name: 'Admin Livia' }
 ];
